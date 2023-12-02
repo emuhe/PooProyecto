c2fccfe6274ce3ceb01e55ed38eee74c68af6e5a
Bryan Zhang <140031745+emuhe@users.noreply.github.com>
2023-12-02 16:18:17 -0500

    Update Main.java

--- src/main/java/ec/edu/espol/Main.java
+++ src/main/java/ec/edu/espol/Main.java
@@ -1,19 +1,48 @@
 package ec.edu.espol;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Juego juego = new Juego();
+        System.out.println("Bienvenido a Domino!\nVas a jugar con alguien (1) o con la maquina (0)?");
+        String tipoJuego = scanner.nextLine();
+        System.out.println("Ingresa el nombre del primer jugador");
+        String j1 = scanner.nextLine();
+        juego.agregarJugador(j1);
+        if(tipoJuego.equals("1")) {
+            System.out.println("Ingresa el nombre del segundo jugador");
+            String j2 = scanner.nextLine();
+            juego.agregarJugador(j2);
+            boolean continuar;
+            do {
+                continuar = juego.Turno(0, scanner);
+                System.out.println("La linea de juego es: ");
+                juego.mostrarLinea();
+                if(continuar) {
+                    juego.Turno(1, scanner);
+                    System.out.println("La linea de juego es: ");
+                    juego.mostrarLinea();
+                }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            } while (continuar);
         }
+        else{
+            boolean continuar;
+            do {
+                juego.agregarJugador("Maquina");
+                continuar = juego.Turno(0, scanner);
+                if(continuar) {
+                    continuar = juego.Turno(1);
+                    System.out.println("La linea de juego es: ");
+                    juego.mostrarLinea();
+                }
+
+            } while (continuar);
+        }
+        System.out.println("\nSe acabo el juego!");
+
+
     }
 }
