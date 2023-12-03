ae644ed69af0353f3506e634f0a1dff33a1b3aa4
Dmolvera <dmolvera@espol.edu.ec>
2023-12-03 04:48:49 -0500

    Create Jugador.java and Utilitario.java

---
+++ src/main/java/ec/edu/espol/Utilitaria.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol;
+
+import java.util.ArrayList;
+
+
+public class Utilitaria {
+        public static ArrayList<Ficha> crearManoJugador(){
+        ArrayList<Ficha> fichas = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            int num1 = (int) (Math.floor(Math.random() * 6) + 1);
+            int num2 = (int) (Math.floor(Math.random() * 6) + 1);
+            Ficha ficha = new Ficha(num1,num2);
+            fichas.add(ficha);
+        }
+        fichas.add(new FichaComodin());
+        return fichas; //Genera la mano del jugador
+    }
+
+}
+    
