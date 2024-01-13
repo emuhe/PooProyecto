3ffa107911002a0b8c120612e1bf8a358ff30a54
Bryan Zhang <140031745+emuhe@users.noreply.github.com>
2024-01-13 16:12:02 -0500

    e

---
+++ src/main/resources/ec/edu/espol/proyectopoofx/PrimaryController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.proyectopoofx;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Deeje
+ */
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private Button primaryButton;
+    @FXML
+    private ComboBox<?> OpcionesdeJugador;
+    
+    private ImageView Imagen_principal_logo;
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        File file = new File("src/Box13.jpg");
+        Image image = new Image(file.toURI().toString());
+        Imagen_principal_logo.setImage(image);
+    }    
+    
+}
