56f6be38d35537604a7a64214bbbb1602d15525a
Bryan Zhang <140031745+emuhe@users.noreply.github.com>
2024-01-13 19:25:07 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ec.edu.espol.proyectopoofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens ec.edu.espol.proyectopoofx to javafx.fxml;
-    exports ec.edu.espol.proyectopoofx;
-}
