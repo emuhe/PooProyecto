6b607fc5f252b6b195470edc4613d9b00e8379a7
P4ND4M <152032772+P4ND4M@users.noreply.github.com>
2024-01-14 21:18:28 -0500

    Update PrimaryController.java

--- src/main/java/ec/edu/espol/proyectopoofx/PrimaryController.java
+++ src/main/java/ec/edu/espol/proyectopoofx/PrimaryController.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -20,8 +19,6 @@ public class PrimaryController {
     @FXML
     private Button primaryButton;
     @FXML
-    private ComboBox<String> OpcionesdeJugador;
-    @FXML
     private ImageView Imagen_principal_logo;
     @FXML
     private Label mensaje_error;
@@ -32,10 +29,9 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException, InterruptedException {
-        String tipoJugador = OpcionesdeJugador.getValue();
         
         boolean name_exists = false;
-        boolean tipe_exists = false;
+        boolean tipe_exists = true;
         String j1 = J1_name.getText();        
         if(!j1.equals("") || check_namej1.isSelected()){
             name_exists = true;
@@ -46,28 +42,19 @@ public class PrimaryController {
         else{
             check_namej1.setVisible(true);       
         }
-        if(tipoJugador == null){
-            mensaje_error.setText("Escoja alguien para jugar");
-        }
-        else{
-            tipe_exists = true;
-        }
         if(name_exists && tipe_exists){
             JuegoFX.NombreJ1 = j1;
-            JuegoFX.tipoj2 = Utilitaria.convertir(tipoJugador);
+            JuegoFX.tipoj2 = Utilitaria.convertir("Computadora");
             App.setRoot("secondary");
         }
     }
     public void initialize(){
         check_namej1.setVisible(false);
-        OpcionesdeJugador.getItems().removeAll(OpcionesdeJugador.getItems());
-        OpcionesdeJugador.getItems().addAll("Computadora", "Jugador");
         File file = new File("src/Domino_Icon.jpeg");
         Image image = new Image(file.toURI().toString());
         Imagen_principal_logo.setImage(image);
     }
 
-    @FXML
     private void actualizara(MouseEvent event) {
         mensaje_error.setText("");
     }
